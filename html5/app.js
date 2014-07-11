4f9b28df4688793153e0143e82a29be50e3387c0
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-07-11 09:58:24 -0400

    code reuse: Surface

--- html5/app.js
+++ html5/app.js
@@ -1,27 +1,17 @@
-var streaming = false, command = "", commandTimeout, state = "none", CANV_WIDTH = Math.floor(1920 * 0.4), CANV_HEIGHT = 1080, gl, gr, g1, g2, orient = "", status2 = "", status3 = "", status4 = "", zeroVector = {x:0,y:0,z:0}, gamma = 0, beta = 0, alpha = 0, upDirection, upC;
+var streaming = false, command = "", commandTimeout, state = "none", CANV_WIDTH = Math.floor(1920 * 0.4), CANV_HEIGHT = 1080, gl, gr, g1, g2, orient = "", zeroVector = {x:0,y:0,z:0}, gamma = 0, beta = 0, alpha = 0, upDirection, upC, vid, leftEye, rightEye, frame1, frame2;
+
 
 function pageLoad() {
     try { 
-        t("vid", "leftEye", "rightEye", "frame1", "frame2");
-        leftEye.width = CANV_WIDTH;
-        rightEye.width = CANV_WIDTH;
-        frame1.width = CANV_WIDTH;
-        frame2.width = CANV_WIDTH;
+        vid = getDOM("#vid");
 
-        leftEye.height = CANV_HEIGHT;
-        rightEye.height = CANV_HEIGHT;
-        frame1.height = CANV_HEIGHT;
-        frame2.height = CANV_HEIGHT;
+        leftEye = new Surface("leftEye");
+        rightEye = new Surface("rightEye");
+        frame1 = new Surface("frame1");
+        frame2 = new Surface("frame2");
 
-        gl = leftEye.getContext("2d");
-        gr = rightEye.getContext("2d");
-        g1 = frame1.getContext("2d");
-        g2 = frame2.getContext("2d");
-
-        gl.font = "20px Arial";
-        gr.font = "20px Arial";
-        //setupSpeech();
-        //setupVideo(vid);
+        setupSpeech();
+        setupVideo(vid);
         setupOrientation(function(g,b,a){
             gamma = g;
             beta = b;
@@ -40,56 +30,30 @@ function cmd_look(){
             var cx = (frame1.width - vid.videoWidth) / 2,
                 cy = (frame1.height - vid.videoHeight) / 2;
 
-            g2.drawImage(frame1, 0, 0, frame1.width, frame1.height);
-            g1.drawImage(vid, cx, cy, vid.videoWidth, vid.videoHeight);
+            frame2.drawImage(frame1, 0, 0);
+            frame1.drawImage(vid, cx, cy);
         }
         catch(exp){
             console.error("While drawing", exp);
         }
     }
-    gl.drawImage(frame1, 0, 0, frame1.width, frame1.height);
-    gr.drawImage(frame2, 0, 0, frame2.width, frame2.height);
+    leftEye.drawImage(frame1, 0, 0);
+    rightEye.drawImage(frame2, 0, 0);
 }
 
 function cmd_none(){
 }
 
-function clearScreen(){
-    gl.fillStyle = "#000000";
-    gr.fillStyle = "#000000";
-    gl.fillRect(0, 0, CANV_WIDTH, CANV_HEIGHT);
-    gr.fillRect(0, 0, CANV_WIDTH, CANV_HEIGHT);
-}
-
-function drawStatus(){
-    var msg = fmt("state: $1\ncommand: $2\n$3", state, command, orient);
-    var y = CANV_HEIGHT / 2 - 100;
-    drawTextBox(msg, y, 25, "#ffffff", gl);
-    drawTextBox(msg, y, 25, "#ffffff", gr);
-}
-
 function draw(){ 
-    clearScreen();
+    leftEye.clear();
+    rightEye.clear();
     var cmd = "cmd_" + state;
     if(window[cmd]){
         window[cmd]();
     }
-    drawStatus();
+    var msg = fmt("state: $1\ncommand: $2\n$3", state, command, orient);
+    var y = CANV_HEIGHT / 2 - 100;
+    leftEye.drawTextBox(msg, y, 25, "#ffffff");
+    rightEye.drawTextBox(msg, y, 25, "#ffffff");
     window.requestAnimationFrame(draw);
-}
-
-function drawTextBox(text, y, lineHeight, color, gfx){
-    gfx.fillStyle = color;
-    text = text.split("\n");
-    var maxWidth = Math.max.apply(Math, text.map(function(line){
-        return gfx.measureText(line).width;
-    }));
-    var cx = 0.5 * (CANV_WIDTH - maxWidth);
-    text.forEach(function(line, i){
-        gfx.fillText(line, cx, y + lineHeight * i);
-    });
-}
-
-function reload(){
-    document.location = document.location.href;
 }
---
+++ html5/surface.js
@@ -0,0 +1,41 @@
+﻿
+function Surface(id, w, h){
+    this.width = w || CANV_WIDTH;
+    this.height = h || CANV_HEIGHT;
+
+    this.canv = getDOM("#" + id);
+    if(!this.canv){
+        this.canv = document.createElement("canvas");
+        this.canv.id = id;
+    }
+    this.canv.width = this.width;
+    this.canv.height = this.height;
+
+    this.gfx = this.canv.getContext("2d");
+    this.gfx.font = "20px Arial";
+}
+
+Surface.prototype.drawImage = function(i, x, y, w, h){
+    i = i.canv || i;
+    w = w || i.width || i.videoWidth;
+    h = h || i.height || i.videoHeight;
+    this.gfx.drawImage(i, x, y, w, h);
+};
+
+Surface.prototype.clear = function(){
+    this.gfx.fillStyle = "#000000";
+    this.gfx.fillRect(0, 0, this.width, this.height);
+}
+
+Surface.prototype.drawTextBox = function(text, y, lineHeight, color){
+    this.gfx.fillStyle = color;
+    text = text.split("\n");
+    var gfx = this.gfx;
+    var maxWidth = Math.max.apply(Math, text.map(function(line){
+        return gfx.measureText(line).width;
+    }));
+    var cx = 0.5 * (this.width - maxWidth);
+    text.forEach(function(line, i){
+        gfx.fillText(line, cx, y + lineHeight * i);
+    });
+}
