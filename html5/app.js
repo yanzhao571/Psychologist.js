c2884edafad9298848c03e83f6321f96b2f9da52
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-07-18 10:35:51 -0400

    more fun

--- html5/app.js
+++ html5/app.js
@@ -1,13 +1,13 @@
-var dg = 0, gamma = 0,
-    db = 0, beta = 0,
-    da = 0, alpha = 0,
+var dg = 0, gamma = 0, tg = 0,
+    db = 0, beta = 0, tb = 0,
+    da = 0, alpha = 0, ta = 0,
     overlay, gfx,
-    camera, scene, effect, renderer, cube,
+    camera, scene, effect, renderer, cube, map,
     clock, firstTime = !isMobile;
 
-function setWidth(w, h) {
-    overlay.width = w;
-    overlay.height = h;
+function setSize(w, h) {
+    overlay.width = 256;
+    overlay.height = 256;
     camera.aspect = w / h;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -19,15 +19,13 @@ function pageLoad() {
     overlay = document.getElementById("overlay");
 
     gfx = overlay.getContext("2d");
-    gfx.font = "25pt Arial";
-    gfx.clearStyle = "rgba(0,0,0,0)";
 
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.y = 1;
     camera.position.z = 10;
 
     renderer = new THREE.WebGLRenderer();
-    overlay.parentElement.insertBefore(renderer.domElement, overlay);
+    overlay.parentElement.replaceChild(renderer.domElement, overlay);
 
     effect = new THREE.StereoEffect(renderer);
     effect.separation = 1;
@@ -40,44 +38,59 @@ function pageLoad() {
             calibrate();
             firstTime = false;
         }
-        setCamera();
     });
 
     window.addEventListener("resize", function () {
-        setWidth(window.innerWidth, window.innerHeight);
+        setSize(window.innerWidth, window.innerHeight);
     }, false);
 
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshLambertMaterial({ color: 0x70ff70, ambient: 0xffffff, opacity:0.75, transparent: true });
-    material.side = THREE.DoubleSide;
+    var material = new THREE.MeshLambertMaterial({color: 0x70ff70, ambient: 0xffffff, opacity:0.75, transparent: true });
     cube = new THREE.Mesh(geometry, material);
 
-    var map = THREE.ImageUtils.loadTexture("nx.jpg");
-    var material2 = new THREE.SpriteMaterial({ map: map, color: 0xffffff, fog: true });
+    map = new THREE.Texture(overlay);//THREE.ImageUtils.loadTexture("nx.jpg");
+    map.needsUpdate = true;
+    var material2 = new THREE.SpriteMaterial({
+        map: map, 
+        color: 0xffffff, 
+        fog: true
+    });
     var sprite = new THREE.Sprite(material2);
     sprite.position.x = - 2;
+    sprite.scale.set(5, 5, 1);
 
     var light = new THREE.DirectionalLight(0xffffff, 0.95);
-    light.position.set(0, 1, 0);
+    var light2 = new THREE.AmbientLight(0x101010);
+    light.position.set(1, 1, 0);
+
+    var material3 = new THREE.LineBasicMaterial({color: 0xff0000});
+    var geometry3 = new THREE.Geometry();
+    for(var a = 0; a < 2 * Math.PI; a += Math.PI / 180){
+        geometry3.vertices.push(new THREE.Vector3(Math.cos(a), a, Math.sin(a)));
+    }
+    var line = new THREE.Line(geometry3, material3);
 
     scene = new THREE.Scene();
     scene.add(light);
+    scene.add(light2);
     scene.add(cube);
     scene.add(sprite);
+    scene.add(line);
 
     
-    setWidth(window.innerWidth, window.innerHeight);
+    setSize(window.innerWidth, window.innerHeight);
     clock.start();
     window.requestAnimationFrame(animate);
 }
 
-function setCamera() { camera.setRotationFromEuler(new THREE.Euler(gamma + dg, alpha + da, beta + db, "YXZ")); }
+function setCamera(dt) {
+    camera.setRotationFromEuler(new THREE.Euler(gamma + dg, alpha + da, beta + db, "YXZ")); 
+}
 
 function calibrate() {
     dg = -gamma;
     db = -beta;
     da = -alpha;
-    setCamera();
 }
 
 function animate() {
@@ -86,11 +99,16 @@ function animate() {
     cube.rotation.x += 0.2 * dt;
     cube.rotation.y += 0.3 * dt;
     cube.rotation.z += 0.5 * dt;
-    effect.render(scene, camera);
+
     gfx.clearRect(0, 0, overlay.width, overlay.height);
     var txt = fmt("g: $1.00, a: $2.00, b: $3.00", gamma, alpha, beta);
-    gfx.fillStyle = "white";
-    for (var dx = 0, lx = overlay.width / 2; dx < 2; ++dx) {
-        gfx.fillText(txt, 100 + dx * lx, 100);
-    }
+    gfx.fillStyle = "#c0c0c0";
+    gfx.fillRect(0, 0, overlay.width, overlay.height);
+    gfx.font = "20px Arial";
+    gfx.fillStyle = "#c00000";
+    gfx.fillText(txt, 10, 20);
+    map.needsUpdate = true;
+
+    setCamera(dt);
+    effect.render(scene, camera);
 }
