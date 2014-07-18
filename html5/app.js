e2d32bc42f1e59fa7389617059a830fc32665fde
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-07-18 11:03:48 -0400

    live video in the scene

--- html5/app.js
+++ html5/app.js
@@ -1,13 +1,33 @@
 var dg = 0, gamma = 0, tg = 0,
     db = 0, beta = 0, tb = 0,
     da = 0, alpha = 0, ta = 0,
-    overlay, gfx,
+    overlay, gfx, vid, vidGfx,
     camera, scene, effect, renderer, cube, map,
     clock, firstTime = !isMobile;
 
+function animate() {
+    var dt = clock.getDelta();
+    window.requestAnimationFrame(animate);
+    cube.rotation.x += 0.2 * dt;
+    cube.rotation.y += 0.3 * dt;
+    cube.rotation.z += 0.5 * dt;
+
+    gfx.clearRect(0, 0, overlay.width, overlay.height);
+    var txt = fmt("g: $1.00, a: $2.00, b: $3.00, fps: $4.0", gamma, alpha, beta, 1/dt);
+    gfx.font = "20px Arial";
+    gfx.fillStyle = "#c00000";
+    gfx.fillText(txt, 10, 20);
+
+    vidGfx.drawImage(vid, 0, 0);
+    map.needsUpdate = true;
+
+    setCamera(dt);
+    effect.render(scene, camera);
+}
+
 function setSize(w, h) {
-    overlay.width = 256;
-    overlay.height = 256;
+    overlay.width = w;
+    overlay.height = h;
     camera.aspect = w / h;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -17,18 +37,7 @@ function setSize(w, h) {
 function pageLoad() {
     clock = new THREE.Clock();
     overlay = document.getElementById("overlay");
-
     gfx = overlay.getContext("2d");
-
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.y = 1;
-    camera.position.z = 10;
-
-    renderer = new THREE.WebGLRenderer();
-    overlay.parentElement.replaceChild(renderer.domElement, overlay);
-
-    effect = new THREE.StereoEffect(renderer);
-    effect.separation = 1;
 
     setupOrientation(function (g, b, a){
         gamma = g;
@@ -40,6 +49,26 @@ function pageLoad() {
         }
     });
 
+    vid = document.createElement("video");
+    vid.autoplay = true;
+    var vidCanv = document.createElement("canvas");
+    vidGfx = vidCanv.getContext("2d");
+
+    setupVideo(640, 480, vid, function(){
+        vidCanv.width = vid.videoWidth;
+        vidCanv.height = vid.videoHeight;
+    });
+
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.y = 1;
+    camera.position.z = 10;
+
+    renderer = new THREE.WebGLRenderer();
+    overlay.parentElement.insertBefore(renderer.domElement, overlay);
+
+    effect = new THREE.StereoEffect(renderer);
+    effect.separation = 1;
+
     window.addEventListener("resize", function () {
         setSize(window.innerWidth, window.innerHeight);
     }, false);
@@ -48,7 +77,7 @@ function pageLoad() {
     var material = new THREE.MeshLambertMaterial({color: 0x70ff70, ambient: 0xffffff, opacity:0.75, transparent: true });
     cube = new THREE.Mesh(geometry, material);
 
-    map = new THREE.Texture(overlay);//THREE.ImageUtils.loadTexture("nx.jpg");
+    map = new THREE.Texture(vidCanv);//THREE.ImageUtils.loadTexture("nx.jpg");
     map.needsUpdate = true;
     var material2 = new THREE.SpriteMaterial({
         map: map, 
@@ -57,7 +86,7 @@ function pageLoad() {
     });
     var sprite = new THREE.Sprite(material2);
     sprite.position.x = - 2;
-    sprite.scale.set(5, 5, 1);
+    sprite.scale.set(4, 4, 1);
 
     var light = new THREE.DirectionalLight(0xffffff, 0.95);
     var light2 = new THREE.AmbientLight(0x101010);
@@ -91,24 +120,4 @@ function calibrate() {
     dg = -gamma;
     db = -beta;
     da = -alpha;
-}
-
-function animate() {
-    var dt = clock.getDelta();
-    window.requestAnimationFrame(animate);
-    cube.rotation.x += 0.2 * dt;
-    cube.rotation.y += 0.3 * dt;
-    cube.rotation.z += 0.5 * dt;
-
-    gfx.clearRect(0, 0, overlay.width, overlay.height);
-    var txt = fmt("g: $1.00, a: $2.00, b: $3.00", gamma, alpha, beta);
-    gfx.fillStyle = "#c0c0c0";
-    gfx.fillRect(0, 0, overlay.width, overlay.height);
-    gfx.font = "20px Arial";
-    gfx.fillStyle = "#c00000";
-    gfx.fillText(txt, 10, 20);
-    map.needsUpdate = true;
-
-    setCamera(dt);
-    effect.render(scene, camera);
 }
---
+++ html5/camera.js
@@ -0,0 +1,52 @@
+﻿function setupVideo(width, height, vid, onPlay){
+    var streaming = false;
+    function getUserMediaFallthrough(vidOpt, err){
+        navigator.getUserMedia({video: vidOpt}, function (stream) {
+            var stream = window.URL.createObjectURL(stream);
+            vid.src = stream;
+        }, err);
+    }
+
+    function connect(source) {
+        try {
+            if (streaming) {
+                if (window.stream) {
+                    window.stream.stop();
+                }
+                vid.src = null;
+                streaming = false;
+            }
+        }
+        catch (err) {
+            console.error("While stopping", err);
+        }
+
+        getUserMediaFallthrough({
+            optional: [{ sourceId: source }],
+            mandatory: {
+                minWidth: width,
+                minHeight: height
+            }
+        }, function(err){
+            console.error("While connecting", err);
+            getUserMediaFallthrough(true, console.error.bind(window, "Final connect attempt"));
+        });
+    }
+
+    vid.addEventListener("canplay", function (ev) {
+        if (!streaming) {
+            streaming = true;
+        }
+    }, false);
+
+    if(onPlay){
+        vid.addEventListener("playing", onPlay, false);
+    }
+    
+    MediaStreamTrack.getVideoTracks(function (infos) {
+
+        //the last one is most likely to be the back camera of the phone
+        //TODO: setup this up to be configurable.
+        connect(infos[infos.length - 1].id);
+    });
+}
