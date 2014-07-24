5f0936101dd9ccf1072bebe5bb05de407103e96b
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-07-23 23:52:28 -0400

    just plain hosed in firefox.

--- html5/motion.js
+++ html5/motion.js
@@ -39,23 +39,19 @@ function calibrateToSensors(){
 }
 
 function setupOrientation(callback) {
-    var ZERO_VECTOR = { x: 9.80665, y: 0, z: 0 }, ZERO_EULER = {gamma: 0, alpha: 0, beta: 0}, ZERO_COORD = {latitude: 0, longitude: 0, altitude: null, accuracy:Number.MAX_VALUE, altitudeAccuracy: null, heading: null, speed: null},
-        acceleration = ZERO_VECTOR, rotation = ZERO_EULER, orientation = ZERO_EULER, location = ZERO_COORD,
+    var ZERO_VECTOR = { x: 9.80665, y: 0, z: 0 }, ZERO_EULER = {gamma: 0, alpha: 0, beta: 0},
+        acceleration = ZERO_VECTOR, rotation = ZERO_EULER, orientation = ZERO_EULER,
         beta = new Angle(0), gamma = new Angle(0), alpha = new Angle(0),
         belowHorizon, landscape, portrait, upsideDown, orientationName,
-        omx, omy, omz, osx, osy, osz, da, db, sb;
-
-    if(!screen.hasOwnProperty("orientation")){
-        screen.orientation = orientationName;
-    }
+        omx, omy, omz, osx, osy, osz, da, db, dg, sb, sg;
 
     function onChange(){
-        landscape = screen.orientation && screen.orientation.indexOf("landscape") == 0;
-        portrait = screen.orientation && screen.orientation.indexOf("portrait") == 0;
-        upsideDown = screen.orientation && screen.orientation.indexOf("secondary") > 0;
+        landscape = orientationName && orientationName.indexOf("landscape") == 0;
+        portrait = orientationName && orientationName.indexOf("portrait") == 0;
+        upsideDown = orientationName && orientationName.indexOf("secondary") > 0;
         if(callback){
             callback({
-                orientation: screen.orientation,
+                orientation: orientationName,
                 isBelowHorizon: belowHorizon,
                 isLandscape: landscape,
                 isPortrait: portrait,
@@ -71,56 +67,51 @@ function setupOrientation(callback) {
                 accelerationZ: acceleration.z,
                 rotationGamma: rotation.gamma,
                 rotationBeta: rotation.beta,
-                rotationAlpha: rotation.alpha,
-                locationAccuracy: location.accuracy,
-                locationLatitude: location.latitude,
-                locationLongitude: location.longitude,
-                locationHeading: location.heading,
-                locationSpeed: location.speed,
-                locationAltitude: location.altitude,
-                locationAltitudeAccuracy: location.altitudeAccuracy
+                rotationAlpha: rotation.alpha
             });
         }
     }
 
     function checkOrientation(event) {        
-        if(landscape){
-            orientation = event || ZERO_EULER;
+        orientation = event || ZERO_EULER;
         
-            belowHorizon = portrait
-                ? (upsideDown ? (orientation.beta >= 90) : (orientation.beta < 90))
-                : (upsideDown ? (orientation.gamma < 0) : (orientation.gamma > 0))
-            if(upsideDown){
-                if(belowHorizon){
-                    da = -90;
-                    sb = 1;
-                }
-                else{
-                    da = 90;
-                    sb = -1;
-                }
-            }
-            else{
-                sb = 1;
-                if(belowHorizon){
-                    da = 90;
-                    sb = -1;
-                }
-                else{
-                    da = -90;
-                    sb = 1;
-                }
-            }
+        belowHorizon = portrait
+            ? (upsideDown ? (orientation.beta >= 90) : (orientation.beta < 90))
+            : (upsideDown ? (orientation.gamma < 0) : (orientation.gamma > 0))
+        if(upsideDown){
+            sg = -1;
             if(belowHorizon){
-                db = 0;
+                da = -90;
+                sb = 1;
             }
             else{
-                db = 180;
+                da = 90;
+                sb = -1;
             }
-            alpha.degrees = orientation.alpha + da;
-            beta.degrees = sb * orientation.beta + db;
-            onChange();
         }
+        else{
+            sg = 1;
+            if(belowHorizon){
+                da = 90;
+                sb = -1;
+            }
+            else{
+                da = -90;
+                sb = 1;
+            }
+        }
+        if(belowHorizon){
+            db = 0;
+            dg = -90;
+        }
+        else{
+            db = 180;
+            dg = 90;
+        }
+        alpha.degrees = orientation.alpha + da;
+        beta.degrees = sb * orientation.beta + db;
+        gamma.degrees = sg * orientation.gamma + dg;
+        onChange();
     }
     window.addEventListener("deviceorientation", checkOrientation, false);
     
@@ -134,37 +125,18 @@ function setupOrientation(callback) {
 
         osx = acceleration.x / omx;
         osy = acceleration.y / omy;
-        osz = acceleration.z / omz;  
+        osz = acceleration.z / omz;
 
-        if(!isFirefox){
-            if(omx > omy && omx > omz && (omx > 4.5 || orientationName == null)){
-                orientationName = "landscape-" + (osx == 1 ? "secondary" : "primary");
-            }
-            else if(omy > omz && omy > omx && (omy > 4.5 || orientationName == null)){
-                orientationName = "portrait-" + (osy == 1 ? "primary" : "secondary");
-            }
-
-            if(screen.orientation != orientationName){
-                screen.orientation = orientationName;
-            }
+        if(omx > omy && omx > omz && (omx > 4.5 || orientationName == null)){
+            orientationName = "landscape-" + (osx == 1 ? "secondary" : "primary");
+        }
+        else if(omy > omz && omy > omx && (omy > 4.5 || orientationName == null)){
+            orientationName = "portrait-" + (osy == 1 ? "primary" : "secondary");
         }
 
         onChange();
     }
     window.addEventListener("devicemotion", checkMotion, false);
-
-    if(isFirefox){
-        screen.addEventListener("orientationchange", onChange);
-    }
-
-    if(navigator.hasOwnProperty("geolocation")){
-        navigator.geolocation.watchPosition(function(event){
-            console.log(event);
-            location = event.coords;
-        }, null, {
-            enableHighAccuracy: true,
-        });
-    }
 
     checkMotion();
     checkOrientation();
