3873efe16ea6d1047206d15f13b697e3ce2dfd07
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-07-23 23:32:57 -0400

    okay, screw portrait mode. And alpha and beta are now working again.

--- html5/motion.js
+++ html5/motion.js
@@ -38,68 +38,94 @@ function calibrateToSensors(){
     calibrated = true;
 }
 
-function setupOrientation(orient, move, rotate, orientName) {
-    var ZERO_VECTOR = { x: 9.80665, y: 0, z: 0 }, ZERO_EULER = {gamma: 0, alpha: 0, beta: 0},
-        acceleration = ZERO_VECTOR, rotation = ZERO_EULER, orientation = ZERO_EULER,
+function setupOrientation(callback) {
+    var ZERO_VECTOR = { x: 9.80665, y: 0, z: 0 }, ZERO_EULER = {gamma: 0, alpha: 0, beta: 0}, ZERO_COORD = {latitude: 0, longitude: 0, altitude: null, accuracy:Number.MAX_VALUE, altitudeAccuracy: null, heading: null, speed: null},
+        acceleration = ZERO_VECTOR, rotation = ZERO_EULER, orientation = ZERO_EULER, location = ZERO_COORD,
         beta = new Angle(0), gamma = new Angle(0), alpha = new Angle(0),
-        belowHorizon, landscape, portrait, upsideDown, orientationName, origOrientName = orientName,
-        omx, omy, omz, osx, osy, osz, da;
+        belowHorizon, landscape, portrait, upsideDown, orientationName,
+        omx, omy, omz, osx, osy, osz, da, db, sb;
 
     if(!screen.hasOwnProperty("orientation")){
         screen.orientation = orientationName;
     }
 
-    function checkOrientation(event) {
-        orientation = event || ZERO_EULER;
-        
-        belowHorizon = portrait 
-            ? (upsideDown ? (orientation.beta < 90) : (orientation.beta >= 90))
-            : (upsideDown ? (orientation.gamma < 0) : (orientation.gamma > 0))
-        
-        if(landscape){
-            if(upsideDown){
-        console.log("Heyo");
-                if(belowHorizon){
-                    da = -90;
-                }
-                else{
-                    da = 90;
-                }
-            }
-            else{
-                if(belowHorizon){
-                    da = 90;
-                }
-                else{
-                    da = -90;
-                }
-            }
-            alpha.degrees = orientation.alpha + da;
-        }
-        else{
-            alpha.degrees = orientation.alpha;
-        }
-
-
-        if(orient){
-            //orient(orientation.gamma, orientation.beta, orientation.alpha);
-            //orient(gamma.degrees, beta.degrees, alpha.degrees);
-            orient(gamma.radians, beta.radians, alpha.radians);
-        }
-    }
-    window.addEventListener("deviceorientation", checkOrientation, false);
-
-    orientName = function(){
+    function onChange(){
         landscape = screen.orientation && screen.orientation.indexOf("landscape") == 0;
         portrait = screen.orientation && screen.orientation.indexOf("portrait") == 0;
         upsideDown = screen.orientation && screen.orientation.indexOf("secondary") > 0;
-        if(origOrientName){
-            origOrientName();
+        if(callback){
+            callback({
+                orientation: screen.orientation,
+                isBelowHorizon: belowHorizon,
+                isLandscape: landscape,
+                isPortrait: portrait,
+                isUpsideDown: upsideDown,
+                pitch: sigfig(gamma.degrees, 2), 
+                roll: sigfig(beta.degrees, 2), 
+                heading: sigfig(alpha.degrees, 2),
+                sensorGamma: orientation.gamma, 
+                sensorBeta: orientation.beta, 
+                sensorAlpha: orientation.alpha,
+                accelerationX: acceleration.x,
+                accelerationY: acceleration.y,
+                accelerationZ: acceleration.z,
+                rotationGamma: rotation.gamma,
+                rotationBeta: rotation.beta,
+                rotationAlpha: rotation.alpha,
+                locationAccuracy: location.accuracy,
+                locationLatitude: location.latitude,
+                locationLongitude: location.longitude,
+                locationHeading: location.heading,
+                locationSpeed: location.speed,
+                locationAltitude: location.altitude,
+                locationAltitudeAccuracy: location.altitudeAccuracy
+            });
         }
-    };
+    }
+
+    function checkOrientation(event) {        
+        if(landscape){
+            orientation = event || ZERO_EULER;
+        
+            belowHorizon = portrait
+                ? (upsideDown ? (orientation.beta >= 90) : (orientation.beta < 90))
+                : (upsideDown ? (orientation.gamma < 0) : (orientation.gamma > 0))
+            if(upsideDown){
+                if(belowHorizon){
+                    da = -90;
+                    sb = 1;
+                }
+                else{
+                    da = 90;
+                    sb = -1;
+                }
+            }
+            else{
+                sb = 1;
+                if(belowHorizon){
+                    da = 90;
+                    sb = -1;
+                }
+                else{
+                    da = -90;
+                    sb = 1;
+                }
+            }
+            if(belowHorizon){
+                db = 0;
+            }
+            else{
+                db = 180;
+            }
+            alpha.degrees = orientation.alpha + da;
+            beta.degrees = sb * orientation.beta + db;
+            onChange();
+        }
+    }
+    window.addEventListener("deviceorientation", checkOrientation, false);
     
     function checkMotion(event) {
-        acceleration = (event && event.x && event.y && event.z && (event.accelerationIncludingGravity || event.acceleration)) || ZERO_VECTOR;
+        acceleration = (event && (event.accelerationIncludingGravity || event.acceleration)) || ZERO_VECTOR;
         rotation = (event && event.rotationRate) || ZERO_EULER;
 
         omx = Math.abs(acceleration.x);
@@ -120,42 +146,26 @@ function setupOrientation(orient, move, rotate, orientName) {
 
             if(screen.orientation != orientationName){
                 screen.orientation = orientationName;
-                orientName();
             }
         }
 
-        if(move){
-            move(acceleration.x, acceleration.y, acceleration.z);
-        }  
-        if(rotate){
-            rotate(rotation.gamma, rotation.beta, rotation.alpha);
-        }
+        onChange();
     }
-    checkMotion();
-    checkOrientation();
     window.addEventListener("devicemotion", checkMotion, false);
 
     if(isFirefox){
-        screen.addEventListener("orientationchange", orientName);
+        screen.addEventListener("orientationchange", onChange);
     }
-}
 
-function motionTester(){
-    var d = findEverything();
-    with(d){
-        b.innerHTML = isChrome ? "Chrome" : (isFirefox ? "Firefox" : "Other");
-        setupOrientation(function(gamma, beta, alpha){
-            og.innerHTML = gamma;
-            ob.innerHTML = beta;
-            oa.innerHTML = alpha;
-        }, function(x, y, z){
-            g.innerHTML = Math.sqrt(x*x + y*y + z*z);
-            ax.innerHTML = x;
-            ay.innerHTML = y;
-            az.innerHTML = z;
-        }, function(gamma, beta, alpha){
-        }, function(){
-            o.innerHTML = screen.orientation;
+    if(navigator.hasOwnProperty("geolocation")){
+        navigator.geolocation.watchPosition(function(event){
+            console.log(event);
+            location = event.coords;
+        }, null, {
+            enableHighAccuracy: true,
         });
     }
+
+    checkMotion();
+    checkOrientation();
 }
