90e948872c2893e8a9a6a87cea3b69925c2253b5
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-07-29 11:50:57 -0400

    major code reorg

--- html5/speech.js
+++ html5/speech.js
@@ -1,43 +1,86 @@
-﻿function setupSpeech(func){
-    var command = "",
-        recognition = new webkitSpeechRecognition(),
-        commandTimeout;
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    recognition.lang = "en-US";
+﻿function Speech(commands){
+    try{
+        var command = "",
+            commandTimeout,
+            running = false,
+            restart = true,
+            recognition = new webkitSpeechRecognition();
 
-    recognition.addEventListener("start", function() {
-        command = ""; 
-    }, true);
+        recognition.continuous = true;
+        recognition.interimResults = true;
+        recognition.lang = "en-US";
 
-    recognition.addEventListener("error", function(event) {
-        command = "speech error"; 
-    }, true);
+        recognition.addEventListener("start", function() {
+            running = true;
+            command = ""; 
+        }, true);
 
-    recognition.addEventListener("end", function() {
-        command = "speech ended";
-        recognition.start();
-    }, true);
+        recognition.addEventListener("error", function(event) {
+            running = false;
+            command = "speech error";
+            if(restart){
+                recognition.start();
+            }
+        }, true);
 
-    recognition.addEventListener("result", function(event) { 
-        if(commandTimeout){
-            clearTimeout(commandTimeout);
-        }
-        var newCommand = arr(event.results).map(function(evt){
-            return evt[0].transcript.trim();
-        }).filter(function(e,i){
-            return i >= event.resultIndex;
-        }).join(" ").trim().toLowerCase();
+        recognition.addEventListener("end", function() {
+            running = false;
+            command = "speech ended";
+            if(restart){
+                recognition.start();
+            }
+        }, true);
 
-        if(newCommand != command){
-            func(newCommand);
-            command = newCommand;
-        }
+        recognition.addEventListener("result", function(event) { 
+            if(commandTimeout){
+                clearTimeout(commandTimeout);
+            }
 
-        commandTimeout = setTimeout(function(){
-            command = "";
-            delete commandTimeout;
-        }, 3000);
-    }, true);
-    recognition.start();
+            var newCommand = arr(event.results).map(function(evt){
+                return evt[0].transcript.trim();
+            }).filter(function(e,i){
+                return i >= event.resultIndex;
+            }).join(" ").trim().toLowerCase();
+
+            if(newCommand != command){
+                command = newCommand;
+                var candidates = commands.filter(function(cmd){ return cmd && cmd.keywords && cmd.keywords.indexOf && cmd.keywords.indexOf(command) > -1;});
+                if(candidates.length == 0){
+                    console.log(fmt("Unknown command: $1", command));
+                }
+                else{
+                    candidates[0].command();
+                }
+            }
+
+            commandTimeout = setTimeout(function(){
+                command = "";
+                delete commandTimeout;
+            }, 2000);
+        }, true);
+
+        this.start = function(){
+            if(!running){
+                restart = true;
+                recognition.start();
+            }
+        };
+
+        this.stop = function(){
+            if(running){
+                restart = false;
+                recognition.stop();
+            }
+        };
+        this.available = true;
+    }
+    catch(err){
+        this.available = false;
+        this.errorMessage = err.message;
+        this.start = this.stop = function(){
+            var msg = fmt("Failed to initialize speech engine. Reason: $1", err.message);
+            console.error(msg);
+            return msg;
+        };
+    }
 }
--- html5/stereohelpercam.js
+++ html5/stereohelpercam.js
@@ -4,34 +4,40 @@
     toggleMenu = function(){},
     changeMode = function(){},
     showSource = function(){},
+    toggleSpeech = function(){},
     toggleReticle = function(){};
 
 function pageLoad(){
-    var camera = document.createElement("video"),
-        overlay = document.getElementById("overlay"),
+    var picture = document.getElementById("picture"),
         reticle = document.getElementById("reticle"),
+        controls = document.getElementById("controls"),
+        options = document.getElementById("options"),
+        captureButton = document.getElementById("captureButton"),
         changeModeButton = document.getElementById("changeModeButton"),
         fullScreenButton = document.getElementById("fullScreenButton"),
         optionsButton = document.getElementById("optionsButton"),
-        captureButton = document.getElementById("captureButton"),
-        controls = document.getElementById("controls"),
-        options = document.getElementById("options"),
-        gfx = overlay.getContext("2d"),
+        reticleButton = document.getElementById("reticleButton"),
+        speechButton = document.getElementById("speechButton"),
+        stdButtonHeight = document.querySelector("a").clientHeight + 8,
+        numMenuItems = document.querySelectorAll("#options>li>a").length,
+        camera = document.createElement("video"),
+        frames = [document.createElement("canvas"), document.createElement("canvas")],
+        fxs = frames.map(function(f){return f.getContext("2d");}),
+        gfx = picture.getContext("2d"),
         frameIndex = 0,
         size = 0,
         rotation = 0,
         pitch = 0,
         dPitch = 0,
-        showReticle = localStorage.getItem("reticle") != "false",
+        n = 0,
+        menuVisible = false,
+        buttonsVisible = true,
+        speech = null,
+        buttonsTimeout = null,
         modes = ["anaglyph", "stereoscope", "crosseye"],
         modeIndex = localStorage.getItem("mode"),
-        n = 0,
-        stdButtonHeight = document.querySelector("a").clientHeight + 8,
-        numMenuItems = document.querySelectorAll("#options>li>a").length,
-        menuVisible = false,
-        buttonsVisible = localStorage.getItem("buttons") != "false",
-        frames = [document.createElement("canvas"), document.createElement("canvas"), document.createElement("canvas")],
-        fxs = frames.map(function(f){return f.getContext("2d");});
+        showReticle = localStorage.getItem("reticle") != "false",
+        useSpeech = localStorage.getItem("speech") == "true";
 
     if(/^\d+$/.test(modeIndex)){
         modeIndex = parseInt(modeIndex, 10);
@@ -40,44 +46,168 @@ function pageLoad(){
         modeIndex = 0;
     }
 
-    function setMenu(){
-        optionsButton.innerHTML = menuVisible ? "[-] options" : "[+] options";
-        options.style.height = px((menuVisible ? numMenuItems : 1) * stdButtonHeight);
+    ////////// menu
+    function showMenu(){
+        if(!menuVisible){
+            toggleMenu();
+        }
     }
 
-    function toggleButtons(){
-        buttonsVisible = !buttonsVisible;
-        localStorage.setItem("buttons", buttonsVisible);
-        controls.style.display = options.style.display = buttonsVisible ? "block" : "none";
+    function hideMenu(){
+        if(menuVisible){ 
+            toggleMenu(); 
+        }
     }
-
-    setMenu();
-
-    window.addEventListener("resize", setMenu);
 
     toggleMenu = function(){
         menuVisible = !menuVisible;
         setMenu();
     };
 
-    goFullscreen = function(){
-        toggleFullScreen();
-        reset();
-    };
+    function setMenu(){
+        optionsButton.innerHTML = menuVisible ? "close options" : "options";
+        options.style.height = px((menuVisible ? numMenuItems : 1) * stdButtonHeight);
+    }
 
+    window.addEventListener("resize", setMenu, false);
+
+    setMenu();
+    
+    ////////// buttons
+    function hideButtonsLater(){
+        if(buttonsTimeout != null){
+            clearTimeout(buttonsTimeout);
+        }
+        buttonsTimeout = setTimeout(hideButtons, 3000);
+    }
+
+    function setButtons(){
+        controls.style.opacity = options.style.opacity = buttonsVisible || menuVisible ? 1 : 0;
+        if(buttonsVisible){
+            hideButtonsLater();
+        }
+    }
+
+    function toggleButtons(){
+        buttonsVisible = !buttonsVisible;
+        setButtons();
+    }
+
+    function showButtons(evt){
+        if(evt){
+            evt.preventDefault();
+        }
+        if(!buttonsVisible){
+            toggleButtons();
+        }
+        hideButtonsLater();
+    }
+
+    function hideButtons(){
+        console.log("hide buttons", buttonsVisible);
+        if(buttonsVisible){
+            toggleButtons();
+        }
+    }
+
+    window.addEventListener("mousemove", showButtons, false);
+    picture.addEventListener("touchend", showButtons, false);
+    
+    setButtons();
+
+    ///////////// reticle
+    toggleReticle = function (){
+        if(modes[modeIndex] != "anaglyph"){
+            showReticle = !showReticle;
+            localStorage.setItem("reticle", showReticle);
+            setReticle();
+        }
+    }
+
+    function setReticle(){
+        reticle.style.display = (showReticle && modes[modeIndex] != "anaglyph") ? "block" : "none";
+        reticleButton.innerHTML = modes[modeIndex] != "anaglyph" 
+            ? showReticle ? "hide reticle" : "show reticle"
+            : "reticle disabled";
+    }
+
+    setReticle();
+    
+    
+    ////////////// speech
+    toggleSpeech = function(){
+        useSpeech = !useSpeech;
+        localStorage.setItem("speech", useSpeech);
+        setSpeech();
+    }
+
+    function setSpeech(){
+        speechButton.innerHTML =
+            speech.available 
+                ? (useSpeech ? "disable speech" : "enable speech")
+                : "speech unavailable";
+        if(useSpeech){
+            speech.start();
+        }
+        else{
+            speech.stop();
+        }
+    }
+
+    speech = new Speech([{
+        keywords: ["capture", "save", "safe", "save picture", "shoot", "snap", "take", "take picture"],
+        command: capture
+    },{
+        keywords: ["reset"],
+        command: reset
+    },{
+        keywords: ["menu", "options"], 
+        command: toggleMenu
+    },{
+        keywords: ["show menu", "show options", "show option", "open menu", "open options", "open option"], 
+        command:  showMenu
+    },{
+        keywords: ["hide menu", "hide options", "hide option", "close menu", "close options", "close option"], 
+        command:  hideMenu
+    },{
+        keywords: ["target"], 
+        command: toggleReticle
+    },{
+        keywords: ["change mode"], 
+        command: changeMode
+    },{
+        keywords: ["stereo"], 
+        command: setMode.bind(window, "stereoscope")
+    },{
+        keywords: ["cross eyed", "cross eye", "crosseyed", "crosseye"], 
+        command: setMode.bind(window, "crosseye")
+    },{
+        keywords: ["green", "green red", "red red", "green green", "red", "red green", "anaglyph"], 
+        command: setMode.bind(window, "anaglyph")
+    }]);
+
+    setSpeech();
+
+    /////////////// window size
+    
+
+    function setSize(){
+        picture.style.height = reticle.style.height = px(picture.clientWidth * camera.videoHeight / camera.videoWidth);
+    }
+
+    window.addEventListener("resize", setSize, false);
+
+    /////////////// drawing
     changeMode = function(btn){
         modeIndex = (modeIndex + 1) % modes.length;
         localStorage.setItem("mode", modeIndex);
         reset();
     };
 
-    setupOrientation(function(evt){
-        rotation = evt.roll;
-        if(frameIndex == 0){
-            pitch = evt.pitch;
-        }
-        dPitch = evt.pitch - pitch;
-    });
+    function setMode(mode){
+        modeIndex = modes.indexOf(mode);
+        reset();
+    }    
     
     capture = function(){
         if(frameIndex < 2){
@@ -88,33 +218,15 @@ function pageLoad(){
             }
         }
         else if(frameIndex == 2){
-            var data = overlay.toDataURL();
+            var data = picture.toDataURL();
 
-            overlay.toBlob(function(blob) {
+            picture.toBlob(function(blob) {
                 var date = new Date().toLocaleString().replace(/\//g, "-");
 		        saveAs(blob, fmt("$1-$2.jpg", modes[modeIndex], date));
             }, "image/jpg");
 
             reset();
         }
-    };
-    
-    reset = function(){
-        size = camera.videoWidth * (modes[modeIndex] == "anaglyph" ? 1 : 0.5);
-        frames.forEach(function(f, i){
-            f.width = size;
-            f.height = camera.videoHeight;
-            fxs[i].clearRect(0, 0, f.width, f.height);
-        });
-        camera.width = overlay.width = camera.videoWidth;
-        camera.height = overlay.height = camera.videoHeight;
-        frameIndex = 0;
-        reticle.style.display = (showReticle && modes[modeIndex] != "anaglyph") ? "block" : "none";
-        controls.style.display = options.style.display = buttonsVisible ? "block" : "none";
-        gfx.clearRect(0,0, overlay.width, overlay.height);
-        changeModeButton.innerHTML = modes[modeIndex];
-        fullScreenButton.innerHTML = isFullScreenMode() ? "windowed" : "fullscreen";
-        captureButton.innerHTML = "capture";
     };
 
     function animate(){
@@ -157,38 +269,44 @@ function pageLoad(){
         }
     }
 
-    function setMode(mode){
-        modeIndex = modes.indexOf(mode);
-        reset();
-    }
+    animate();
 
-    toggleReticle = function (){
-        showReticle = !showReticle;
-        reticle.style.display = (showReticle && modes[modeIndex] != "anaglyph") ? "block" : "none";
-        localStorage.setItem("reticle", showReticle);
-    }
+    /////////////// the rest
+
+    goFullscreen = function(){
+        toggleFullScreen();
+        reset();
+    };
+
+    setupOrientation(function(evt){
+        rotation = evt.roll;
+        if(frameIndex == 0){
+            pitch = evt.pitch;
+        }
+        dPitch = evt.pitch - pitch;
+    });
+    
+    reset = function(){
+        size = camera.videoWidth * (modes[modeIndex] == "anaglyph" ? 1 : 0.5);
+        frames.forEach(function(f, i){
+            f.width = size;
+            f.height = camera.videoHeight;
+            fxs[i].clearRect(0, 0, f.width, f.height);
+        });
+        camera.width = picture.width = camera.videoWidth;
+        camera.height = picture.height = camera.videoHeight;
+        frameIndex = 0;
+        setReticle();
+        setSize();
+        gfx.clearRect(0,0, picture.width, picture.height);
+        changeModeButton.innerHTML = modes[modeIndex];
+        fullScreenButton.innerHTML = isFullScreenMode() ? "windowed" : "fullscreen";
+        captureButton.innerHTML = "capture";
+    };
 
     var videoModes = isMobile 
         ? [{w:640, h:480}, "default"]
         : [{w:1920, h:1080}, {w:1280, h:720}, {w:1024, h:768}, {w:640, h:480}, "default"];
     videoModes.push("default");
     setupVideo(videoModes, camera, reset);
-    setupSpeech(function(command){
-        console.log(command);
-        switch(command){
-            case "capture": case "save": case "safe": case "save picture": case "shoot": case "snap": case "take": case "take picture": capture(); break;
-            case "reset": reset(); break;
-            case "menu": case "options": toggleMenu(); break;
-            case "open menu": case "open options": if(!menuVisible){ toggleMenu(); } break;
-            case "close menu": case "close options": if(menuVisible){ toggleMenu(); } break;
-            case "change mode": changeMode(); break;
-            case "stereo": setMode("stereoscope"); break
-            case "cross eyed": case "cross eye": case "crosseyed": case "crosseye": setMode("crosseye"); break
-            case "green": case "green red": case "red red": case "green green": case "red": case "red green": case "anaglyph": setMode("anaglyph"); break
-            case "target": toggleReticle(); break;
-            case "hide button": case "hide buttons": if(buttonsVisible) { toggleButtons(); } break;
-            case "show button": case "show buttons": if(!buttonsVisible) { toggleButtons(); } break;
-        }
-    });
-    animate();
 }
