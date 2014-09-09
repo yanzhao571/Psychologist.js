f8cef0b80922036dc1318ac18ef6ce04bc917a1c
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-09-09 16:18:48 -0400

    #7: as many clients as the user wants

--- src/webSocketServer.js
+++ src/webSocketServer.js
@@ -1,56 +1,56 @@
-﻿var pairs = {};
+﻿var types = ["gamepad", "keyboard", "mouse", "touch", "motion", "speech"],
+    socketGroups = {};
 
-function proxyCommand(key, there, command, commandState){
-    if(pairs[key][there]){
-        pairs[key][there].emit(command, commandState);
+function proxyCommand(key, command, commandState){
+    for(var i = 0; i < socketGroups[key].length; ++i){
+        if(i != this.index){
+            socketGroups[key][i].emit(command, commandState);
+        }
     }
 }
 
-var types = ["gamepad", "keyboard", "mouse", "touch", "motion", "speech"];
 
 module.exports = function (socket) {
-    socket.on("key", function (key) {
-        var here, there, pair;
-        
-        if(!pairs[key]){
-            pairs[key] = {};
-        }
+    var group, lastKey;
 
-        pair = pairs[key];
-
-        if(!pair.left){
-            here = "left";
-            there = "right";
-        }
-        else if(!pair.right){
-            here = "right";
-            there = "left";
-        }
-        else{
-            socket.emit("bad");
-        }
-
-        if(here && there){
-            pair[here] = socket;
-
-            types.forEach(function(t){
-                socket.on(t, proxyCommand.bind(socket, key, there, t));
-            });
-
-            socket.on("disconnect", function(){
-                delete pair[here];
-                if(pair[there]){
-                    pair[there].emit("close");
-                }
-                else{
-                    delete pairs[key];
-                    pair = null;
-                }
-            });
-            socket.emit("good", here);
-            if(pair[there]){
-                pair[there].emit("open");
+    function disconnect(){
+        if(group){
+            group.splice(socket.index, 1);
+            for(var i = 0; i < group.length; ++i){
+                group[i].index = i;
+                group[i].emit("close", socket.index);
             }
+            if(group.length == 0){
+                delete socketGroups[lastKey];
+                group = null;
+            }
+        }
+    }
+
+    socket.on("key", function (key) {
+        if(lastKey){
+            disconnect();
+            lastKey = null;
+        }
+
+        lastKey = key;
+        if(!socketGroups[key]){
+            socketGroups[key] = [];
+        }
+
+        group = socketGroups[key];
+
+        socket.index = group.length;
+        group.push(socket);
+
+        types.forEach(function(t){
+            socket.on(t, proxyCommand.bind(socket, key, t));
+        });
+
+        socket.on("disconnect", disconnect);
+        socket.emit("good", {index: socket.index, total: group.length});
+        for(var i = 0; i < group.length && i < socket.index; ++i){
+            group[i].emit("open", {index: socket.index, total: group.length});
         }
     });
 }
