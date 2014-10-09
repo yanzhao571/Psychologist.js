1adcd087c0544c832da74bde6888a5c006ee1599
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-10-09 18:12:35 -0400

    USELESS physics code

--- html5/js/ModelLoader.js
+++ html5/js/ModelLoader.js
@@ -2,15 +2,41 @@ var COLLADA = new THREE.ColladaLoader();
 COLLADA.options.convertUpAxis = true;
 function ModelLoader(src, progress, success){
     if(src){
-        ModelLoader.loadCollada(src, progress, function(object){
-            this.template = object;
-            object.traverse(function(child){
+        ModelLoader.loadCollada(src, progress, function(objects){
+            this.template = objects;
+            objects.traverse(function(child){
                 if(child.name){
                     this[child.name] = child;
+                    var mesh = child.children[0];
+                    if(mesh instanceof THREE.Mesh){
+                        mesh.geometry.computeBoundingBox();
+                        var delta = new THREE.Vector3().subVectors(
+                            mesh.geometry.boundingBox.max, 
+                            mesh.geometry.boundingBox.min
+                        ).multiplyScalar(0.5);
+                        var shape = new CANNON.Box(new CANNON.Vec3(
+                            delta.x, 
+                            delta.y, 
+                            delta.z
+                        ));
+                        var body = new CANNON.Body({
+                            mass: 1
+                        });
+                        body.position.copy(child.position);
+                        body.addShape(shape);
+                        body.angularVelocity.set(0, 0, 0);
+                        body.angularDamping = 0.5;
+                        body.returnState = function(){
+                            child.position.copy(body.position);
+                            child.quaternion.copy(body.quaternion);
+                        };
+                        child.physicsBody = body;
+                        
+                    }
                 }
             }.bind(this));
             if(success){
-                success(object);
+                success(objects);
             }
         }.bind(this));
     }
