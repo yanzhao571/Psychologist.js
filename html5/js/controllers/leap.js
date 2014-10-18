2d54263e6fe4dfcba0a9b8690e12971b60e657cf
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-10-17 20:31:54 -0400

    all the hand parts

--- html5/js/controllers/leap.js
+++ html5/js/controllers/leap.js
@@ -4,6 +4,7 @@ function (){
 var WIDTH = 640,
     HEIGHT = 480,
     HAND_COLORS = ["blue", "green"],
+    FINGER_PARTS = ["tip", "dip", "pip", "mcp", "carp"],
     c = findEverything(),
     g = c.canv.getContext("2d"),
     controller = new Leap.Controller(),
@@ -25,13 +26,12 @@ function E(v, f){
 
 function leapInterpolate(frame, pos){
     var b = frame.interactionBox,
-        top = b.center[1] + b.size[1] * 0.5,
-        left = b.center[0] - b.size[0] * 0.5,
+        left = b.center[0] - b.width,
+        top = b.center[1] + b.height,
         x = pos[0] - left,
         y = pos[1] - top;
-
-    x *= WIDTH / b.size[0];
-    y *= HEIGHT / b.size[1];
+    x *= WIDTH / (2 * b.width);
+    y *= HEIGHT / (2 * b.height);
     
     return {x: x, y: -y};
 }
@@ -51,16 +51,17 @@ E("frame", function(frame){
     g.clearRect(0, 0, WIDTH, HEIGHT);
     g.fillStyle = "black";
     g.fillRect(0, 0, WIDTH, HEIGHT);
-    for( var h = 0; h < frame.hands.length; h++ ){
+    for(var h = 0; h < frame.hands.length; ++h){
         g.fillStyle = HAND_COLORS[h];
         var hand = frame.hands[h];
         var p = leapInterpolate(frame, hand.palmPosition);
-        g.fillRect(p.x, p.y, 20, 20);
-        g.fillStyle = "red";
-        for( var f = 0; f < hand.fingers.length; f++ ){
+        g.fillRect(p.x, p.y, 25, 25);
+        for(var f = 0; f < hand.fingers.length; ++f){
             var finger = hand.fingers[f];
-            p = leapInterpolate(frame, finger.tipPosition);
-            g.fillRect(p.x, p.y, 10, 10);
+            for(var i = 0; i < FINGER_PARTS.length; ++i){
+                p = leapInterpolate(frame, finger[FINGER_PARTS[i] + "Position"]);
+                g.fillRect(p.x, p.y, 10, 10);
+            }
         }
     }
 });
