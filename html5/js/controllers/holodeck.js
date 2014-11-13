0cf65bd9d8cb39c9a2cc2cef422e0a3973ebbbd7
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-11-12 22:04:13 -0500

    starting on a tone gen demo

--- html5/js/controllers/holodeck.js
+++ html5/js/controllers/holodeck.js
@@ -14,25 +14,43 @@ function holodeck(){
         "music/ambient.mp3"
     );
     
+    var TONE_COUNT = 10;
+    var NOTE_COUNT = 8;
+    var CUBE_SIZE = 1;
+    var cubes = [];
+    for(var y = 0; y < NOTE_COUNT; ++y){
+        cubes[y] = [];
+        for(var x = 0; x < TONE_COUNT; ++x){
+            cubes[y][x] = box(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, 0xffffff);
+        }
+    }
+    
     app.addEventListener("ready", function(){
+        this.currentUser.position.z += -19;
         // add a bunch of buttons to the scene that we can play with
-        var COUNT = 5;
-        for(var i = -COUNT; i <= COUNT; ++i){
+        for(var x = 0; x < TONE_COUNT; ++x){
             // I want some to be momentary and some to be toggle.
-            var btn = this.makeButton((i+COUNT)%2);
+            var btn = this.makeButton((x+TONE_COUNT)%2);
             this.scene.add(btn.base);
             
             // Let's place the buttons in an arc, with a stagger of height
             // to make a sort of keyboard
-            var angle = Math.PI * i * 10 / 180;
+            var angle = Math.PI * (x - TONE_COUNT / 2) * 10 / 180 + Math.PI;
             var r = 10;
-            btn.position.set(Math.cos(angle) * r, Math.cos(i * Math.PI) * 0.25, Math.sin(-angle) * r);
-            btn.rotation.set(0, angle - Math.PI, 0, "XYZ");
+            btn.position.set(Math.sin(angle) * r, Math.cos(x * Math.PI) * 0.25, Math.cos(angle) * r + 16);
+            btn.rotation.set(0, angle+Math.PI/2, 0, "XYZ");
             
             // And let's play notes in a pentatonic scale!
             btn.addEventListener("click", function(n){
                 this.audio.sawtooth(40 - n * 5, 0.1, 0.25);
-            }.bind(this, i));
+            }.bind(this, x));
+            
+            // now, let's make a visualizer.
+            for(var y = 0; y < NOTE_COUNT; ++y){
+                var b = cubes[y][x];
+                b.position.set(2 * CUBE_SIZE * (x - TONE_COUNT / 2), (y + 3) * 2 * CUBE_SIZE, -12);
+                this.scene.add(b);
+            }
         }
     }.bind(app));
 
