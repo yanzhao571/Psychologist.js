1411f3dd6ad971fe9cd43d67bfe6788050aa8c8d
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2015-01-12 13:53:42 -0500

    importing Primrose

---
+++ config.js
@@ -0,0 +1,14 @@
+System.config({
+  "paths": {
+    "*": "*.js",
+    "Psychologist/*": "src/*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "Primrose": "github:capnmidnight/Primrose@master"
+  }
+});
+
